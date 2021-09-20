56c45f1703008194a68c92e4c85db7df237af04d
Matesoidicek <mat.dusek@gmail.com>
2021-09-20 22:00:37 +0200

    First input bars

--- index.py
+++ index.py
@@ -1,3 +1,18 @@
 import pyautogui as py
 import webbrowser as web
 import time 
+
+# message
+message = input("what message do you want to spam? ")
+
+#how many times
+repeats = int(input("How many times do you want to spam this message? "))
+
+#delay
+delay = int(input("How many ms do you wanna wait between each message? "))
+
+ready = input("Press enter when you are ready! ")
+
+#When you are ready
+print("You have five seconds to be ready for spam! ")
+
